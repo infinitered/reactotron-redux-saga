0ec970f5cf977116d9dc11586d371076e466e97c
Richard Evans <rmevans9@gmail.com>
2019-02-04 20:50:36 -0500

    A few more tests

---
+++ src/helpers/isRaceEffect.test.ts
@@ -0,0 +1,14 @@
+import * as Effects from "redux-saga/effects"
+
+import { isRaceEffect } from "./isRaceEffect"
+import { createMockTask } from "@redux-saga/testing-utils"
+
+describe("isRaceEffect", () => {
+  it("should detect a race effect", () => {
+    expect(isRaceEffect(Effects.race([createMockTask()]))).toBeTruthy()
+  })
+
+  it("should tell us something isn't a race effect", () => {
+      expect(isRaceEffect(Effects.take())).toBeFalsy();
+  })
+})
