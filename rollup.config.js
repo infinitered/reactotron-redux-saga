09951362979c2c2277d0cb07bb05c869f2fcf1e3
Richard Evans <rmevans9@gmail.com>
2019-02-24 07:13:06 -0500

    Allow for non minified builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "rollup-plugin-node-resolve"
 import babel from "rollup-plugin-babel"
 import filesize from "rollup-plugin-filesize"
-import minify from 'rollup-plugin-babel-minify';
+import minify from "rollup-plugin-babel-minify"
 
 export default {
   input: "src/index.ts",
@@ -12,9 +12,11 @@ export default {
   plugins: [
     resolve({ extensions: [".ts"] }),
     babel({ extensions: [".ts"], runtimeHelpers: true }),
-    minify({
-      comments: false,
-    }),
+    process.env.NODE_ENV === "production"
+      ? minify({
+          comments: false,
+        })
+      : null,
     filesize(),
   ],
   external: ["@redux-saga/is", "redux-saga/effects", "@babel/runtime/helpers/esm/extends"],
