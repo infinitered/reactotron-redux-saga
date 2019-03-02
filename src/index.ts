eefb1172f2fb1666a4f1776ed94df523a54d78b4
rmevans9 <rmevans9@gmail.com>
2019-03-01 21:39:40 -0500

    feat: Better TypeScript Typings

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
+import { Reactotron } from "reactotron-core-client"
+import { SagaMonitor } from "redux-saga";
+
 import createSagaMonitor, { PluginConfig } from "./sagaMonitor"
-import { Reactotron } from "reactotron-react-native"
 
 export default (pluginConfig: PluginConfig) => (reactotron: Reactotron) => ({
   features: {
     createSagaMonitor: (options: any) => createSagaMonitor(reactotron, options, pluginConfig),
   },
 })
+
+declare module "reactotron-react-native" {
+  // eslint-disable-next-line import/export
+  export interface Reactotron {
+    createSagaMonitor(): SagaMonitor
+  }
+}
+
+declare module "reactotron-react-js" {
+  // eslint-disable-next-line import/export
+  export interface Reactotron {
+    createSagaMonitor(): SagaMonitor
+  }
+}
